c9686528a85d15621f9167321c1edffd5d169a1f
y119777 <1197771872@qq.com>
2018-02-28 22:03:17 +0800

    change config

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY', 'AIzaSyD4Zgm65H1uincYyU0dyCU5m4a9WKHgn_c');
 define('GJ_CODE', 'US');
 define('SITE_NAME','c1');
 define('TITLENAME', 'c2');
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('EN2DEKEY', 'AIzaSyD4Zgm65H1uincYyU0dyCU5m4a9WKHgn_c');
+define('EMAIL', 'y1197771872@gmail.com');
 ?>
