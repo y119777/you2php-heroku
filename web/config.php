13a1848cf916aef18de155cc40c545ac47373af1
y119777 <1197771872@qq.com>
2018-02-28 21:58:37 +0800

    change title

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
+define('GJ_CODE', 'US');
+define('SITE_NAME','c1');
+define('TITLENAME', 'c2');
 define('EN2DEKEY',getenv("EN2DEKEY"));
 define('EMAIL', getenv("EMAIL"));
 ?>
